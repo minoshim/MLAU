f4fe6a9bf05dcb292738a03d90a658cf8b7080c3
MINOSHIMA Takashi <minoshim@etude.yes.jamstec.go.jp>
2020-04-22 16:25:10 +0900

    modify 2D MPI files

--- 2D/MPI/KHI/mpi_bc2d.c
+++ 2D/MPI/KHI/mpi_bc2d.c
@@ -15,6 +15,34 @@
 /* You should have received a copy of the GNU General Public License */
 /* along with MLAU.  If not, see <https://www.gnu.org/licenses/>. */
 
+void mpi_xbc2d(double *f, int nx, int ny, int xoff, int yoff, int st, int dn,
+	       int mpi_rank, int mpi_numx, int mpi_numy);
+void mpi_ybc2d(double *f, int nx, int ny, int xoff, int yoff, int st, int dn,
+	       int mpi_rank, int mpi_numx, int mpi_numy);
+
+void mpi_xbc2d(double *f, int nx, int ny, int xoff, int yoff, int st, int dn,
+	       int mpi_rank, int mpi_numx, int mpi_numy)
+/* 2D X Dirichlet or Neumann BC under MPI */
+/* st: Flag for staggered grid. Set 1 when f is @ cell face (not center) */
+/* dn: Factor of Dirichlet (-1) or Neumann (+1). if dn==0, nothing to do */
+{
+  if (dn != 0){
+    int i,j;
+    /* Left */
+    if ((mpi_rank % mpi_numx) == 0){
+      for (j=0;j<ny;j++){
+	for (i=0;i<xoff;i++) f[nx*j+i]=dn*f[nx*j+(2*xoff-1+st)-i];
+      }
+    }
+    /* Right */
+    if ((mpi_rank % mpi_numx) == (mpi_numx-1)){
+      for (j=0;j<ny;j++){
+	for (i=0;i<xoff;i++) f[nx*j+(nx-1-i)]=dn*f[nx*j+(nx-2*xoff+st)+i];
+      }
+    }
+  }
+}
+
 void mpi_ybc2d(double *f, int nx, int ny, int xoff, int yoff, int st, int dn,
 	       int mpi_rank, int mpi_numx, int mpi_numy)
 /* 2D Y Dirichlet or Neumann BC under MPI */
