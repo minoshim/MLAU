9eb7c7b2a50d05667b72d3b27a2b4f7002abd38d
MINOSHIMA Takashi <minoshim@etude.yes.jamstec.go.jp>
2020-07-13 19:36:00 +0900

    Create MRX directory for magnetic reconnection

---
+++ 2D/MPI/MRX/bkup_save.hpp
@@ -0,0 +1,52 @@
+void bkup_save(int n, int cnt, double tim, double dt, double trec, int nall, int mpi_rank)
+// Save bkup files
+{
+  FILE *outfil;
+  double vald[]={tim,dt,trec};
+  char filname[100];
+
+  if (mpi_rank == 0){
+    sprintf(filname,"%s/bkup_stamp.dat",fildir);
+    outfil=fopen(filname,"w");
+    fprintf(outfil,"%d %d\n",n,cnt);
+    fclose(outfil);
+
+    sprintf(filname,"%s/bkup_time.dat",fildir);
+    outfil=fopen(filname,"wb");
+    fwrite(&vald,sizeof(double),3,outfil);
+    fclose(outfil);
+  }
+
+  sprintf(filname,"%s/bkup_ro_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(ro,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_mx_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(mx,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_my_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(my,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_mz_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(mz,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_bx_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(bx,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_by_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(by,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_bz_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(bz,sizeof(double),nall,outfil);
+  fclose(outfil);
+  sprintf(filname,"%s/bkup_en_%05d.dat",fildir,mpi_rank);
+  outfil=fopen(filname,"wb");
+  fwrite(en,sizeof(double),nall,outfil);
+  fclose(outfil);
+}
---
+++ 2D/MPI/MRX/dataio.hpp
@@ -0,0 +1,76 @@
+inline void conv_d2f(double vali[], float valo[], unsigned long nn)
+// convert double to single precision
+{
+  unsigned long i;
+  for (i=0;i<nn;i++) valo[i]=(float)vali[i];
+}
+
+void dataio(int n, int cnt, double tim, int mpi_rank)
+{
+  int i;
+  int nall=nx*ny;
+  FILE *outfil;
+  char filname[100];
+  float *val;
+  val=new float[nall];
+
+  if (n == 0){
+    if (mpi_rank == 0){
+      sprintf(filname,"%s/params.dat",fildir);
+      outfil=fopen(filname,"w");
+      fprintf(outfil,"%f %f %f %f %f\n",gam,beta,lambda,eta0,nu0);
+      fclose(outfil);
+      
+      sprintf(filname,"%s/offsets.dat",fildir);
+      outfil=fopen(filname,"w");
+      fprintf(outfil,"%d %d\n",XOFF,YOFF);
+      fclose(outfil);
+
+      sprintf(filname,"%s/t.dat",fildir);
+      outfil=fopen(filname,"w");
+      fprintf(outfil,"%f\n",tim);
+      fclose(outfil);
+    }
+    if ((mpi_rank/mpi_numx) == 0){
+      sprintf(filname,"%s/x_%05d.dat",fildir,(mpi_rank%mpi_numx));
+      outfil=fopen(filname,"w");
+      for (i=0;i<nx;i++) fprintf(outfil,"%.12f\n",x[i]);
+      fclose(outfil);
+    }
+    if ((mpi_rank%mpi_numx) == 0){
+      sprintf(filname,"%s/y_%05d.dat",fildir,(mpi_rank/mpi_numx));
+      outfil=fopen(filname,"w");
+      for (i=0;i<ny;i++) fprintf(outfil,"%.12f\n",y[i]);
+      fclose(outfil);
+    }
+  } else{
+    if (mpi_rank == 0){
+      sprintf(filname,"%s/t.dat",fildir);
+      outfil=fopen(filname,"a");
+      fprintf(outfil,"%f\n",tim);
+      fclose(outfil);
+    }
+  }
+
+  sprintf(filname,"%s/outdat_%05d_%05d.dat",fildir,cnt,mpi_rank);
+  outfil=fopen(filname,"wb");
+  conv_d2f(ro,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(mx,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(my,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(mz,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(en,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(bx,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(by,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  conv_d2f(bz,val,nall);
+  fwrite(val,sizeof(float),nall,outfil);
+  fclose(outfil);
+
+  delete[] val;
+}
