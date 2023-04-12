17d6511e6aa9c036f63664638bc68d26aa956d30
Minoshima Takashi <minoshim@jamstec.go.jp>
2023-04-12 14:55:31 +0900

    minor change

--- 3D/MPI/OTvortex/global.hpp
+++ 3D/MPI/OTvortex/global.hpp
@@ -8,16 +8,16 @@
 #include <time.h>
 #include "myfunc.h"
 
-#define XMESH (100)		// Number of cells in X domain
-#define YMESH (100)		// Number of cells in Y domain
-#define ZMESH (100)		// Number of cells in Z domain
+#define XMESH (200)		// Number of cells in X domain
+#define YMESH (200)		// Number of cells in Y domain
+#define ZMESH (4)		// Number of cells in Z domain
 #define XOFF (4)		// Number of ghost cells in each X side.
 #define YOFF (4)		// Number of ghost cells in each Y side.
 #define ZOFF (4)		// Number of ghost cells in each Z side.
 
-#define MNP_X (2)		// Number of MPI processes in X
-#define MNP_Y (2)		// Number of MPI processes in Y
-#define MNP_Z (2)		// Number of MPI processes in Z
+#define MNP_X (4)		// Number of MPI processes in X
+#define MNP_Y (4)		// Number of MPI processes in Y
+#define MNP_Z (1)		// Number of MPI processes in Z
 #define MNP (MNP_X*MNP_Y*MNP_Z)	// Total number of MPI processes
 
 #define CFLCHECK (0)		// Flag to modify dt at every step
@@ -33,11 +33,11 @@ namespace global
   const double dtor=pi/180.;
 
   // Parameters
-  int nrec=75;			// Number of iterations for output
+  int nrec=150;			// Number of steps for output
   int nmax=nrec*5;		// Number of maximum iteration
-  const double dtrec=0.2*pi;	// Time step for output
+  const double dtrec=0.2*pi; 	// Time step for output  
   double trec=dtrec;
-  
+
   const double lx=2.0*pi;	// Spatial domain in X
   const double ly=2.0*pi;	// Spatial domain in Y
   const double lz=2.0*pi;	// Spatial domain in Z
